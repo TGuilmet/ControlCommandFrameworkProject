08862d5eb3e838059c9d65c94bae7baeab167a17
theo59000 <guilmettheo@gmail.com>
2026-05-01 18:29:27 +0200

    changes in matrix header

--- include/math/matrix.h
+++ include/math/matrix.h
@@ -3,28 +3,37 @@
 
 #include <common.h>
 
+#define MATRIX_SUCCESS 0
+#define MATRIX_ERROR -1
+
 typedef struct {
     double *data;
     size_t rows;
     size_t cols;
 } matrix_t;
 
-//Constructeur
-void construct_matrix(matrix_t *m);
+//Constructor
+int construct_matrix(matrix_t *m, size_t rows, size_t cols);
+
+//Descructor
+void free_matrix(matrix_t *m);
+
+// Copy
+void copy_matrix(matrix_t*R, matrix_t *m);
 
 // Acces to element (i,j)
-double matrix_get(const matrix_t *m, size_t i, size_t j);
+double get_value_matrix(const matrix_t *m, size_t i, size_t j);
 
 // Set element (i,j)
-void matrix_set(matrix_t *m, size_t i, size_t j, double value);
+void set_value_matrix(matrix_t *m, size_t i, size_t j, double value);
 
 // Addition of matrices
-int add_matrix(matrix_t *a, const matrix_t *b);
+int add_matrix(matrix_t *R, matrix_t *A, const matrix_t *B);
 
 // Multiply by a scalar
 void scale_matrix(matrix_t *m, double k);
 
 //Multiply two matrices
-int multiply(matrix_t *A, matrix_t *B);
+int multiply(matrix_t *R, matrix_t *A, matrix_t *B);
 
 #endif
