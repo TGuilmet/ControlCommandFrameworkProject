ef5f2da5b5f5f2d0ddeda89e0e9e5e2fa2caaccd
theo59000 <guilmettheo@gmail.com>
2026-05-01 22:21:42 +0200

    clone in header

--- include/math/matrix.h
+++ include/math/matrix.h
@@ -6,6 +6,8 @@
 #define MATRIX_SUCCESS 0
 #define MATRIX_ERROR -1
 
+#define MAT(m,i,j) ((m)->data[(i)*(m)->cols + (j)])
+
 typedef struct {
     double *data;
     size_t rows;
@@ -18,8 +20,13 @@ int matrix_construct(matrix_t *m, size_t rows, size_t cols);
 //Descructor
 void matrix_free(matrix_t *m);
 
-// Copy
-void matrix_copy(matrix_t*R, const matrix_t *m);
+// Copy a matrix but size should be equal
+// R should always be different from m
+int matrix_copy(matrix_t *R, const matrix_t *m);
+
+// Copy and reallocate if the size are different.
+//R should always be different from m
+int matrix_clone(matrix_t *R, const matrix_t *m);
 
 // Acces to element (i,j)
 double matrix_get(const matrix_t *m, size_t i, size_t j);
@@ -28,12 +35,14 @@ double matrix_get(const matrix_t *m, size_t i, size_t j);
 void matrix_set(matrix_t *m, size_t i, size_t j, double value);
 
 // Addition of matrices
+// R should always be different from A and B
 int matrix_add(matrix_t *R, const matrix_t *A, const matrix_t *B);
 
 // Multiply by a scalar
 void matrix_scale(matrix_t *m, double k);
 
 //Multiply two matrices
+// R should always be different from A and B
 int matrix_multiply(matrix_t *R, const matrix_t *A, const matrix_t *B);
 
 #endif
